0292863039e07980daf6ff82ec79d2d1495260e2
ito.hitoshi81 <ito.hitoshi81@gmail.com>
2023-12-01 13:51:22 +0900

    Header,Footerを作成

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <footer class="footer">
+    <small>&copy; my-vite-todo</small>
+  </footer>
+</template>
+
+<style>
+.footer {
+  margin-top: 15px;
+  height: 30px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <h1 class="title">TODO</h1>
+</template>
+
+<style scoped>
+.title {
+  width: 100%;
+  background-color: #e3f2fd;
+  text-align: center;
+  font-size: 32px;
+  font-weight: bold;
+  padding: 8px 0;
+}
+</style>
